7f381ce69627fc77e4e95fd8fc613c02785cdb00
ntflix <felix@iron59.co.uk>
2026-07-07 20:05:51 +0100

    fix trailing bracket

--- InstrumentUI.py
+++ InstrumentUI.py
@@ -52,7 +52,7 @@ class InstrumentUI(Focusable):
         self.held_buttons: set[Any] = set()
         self.instrument = instrument
         self._onMusicEvent = onMusicEvent
-        eventbus.emit(PurpleRandom))
+        eventbus.emit(PurpleRandom)
 
     def _note_event_for_button(self, button_name: str) -> MusicEvent:
         event = self.BUTTON_VALUES.get(button_name)
